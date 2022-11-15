7d154e06c6d74d53384da2f1b54def42578a72cd
shaonkislo <shaon6340@agmail.com>
2022-11-15 20:03:19 +0600

    initialize

---
+++ src/_auth-steps.js
@@ -0,0 +1,16 @@
+/* 
+01. create a new project in console.firebase.google.com  
+02. npm install firebase
+03. create firebase.init.js and import getAuth to export auth
+04. Firebase settings > Authentication > enable Email And password auth
+*/
+
+
+/* 
+** hosting steps 
+01. npm install -g firebase-tools 
+02. firebase login
+03. firebase init
+04. firebase deploy 
+** প্রথম ভিডিওএ ৭ মিনিটের দিকে... এগুলো পরে কাজে লাগবে।  
+*/
